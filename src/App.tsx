8bbee79a82b821820a3d2af453478ca1cd3a0812
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-06-28 09:01:22 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RootNavigator from './navigation/RootNavigator';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <RootNavigator />
+    </>
+  );
+}
+
+export default App;
